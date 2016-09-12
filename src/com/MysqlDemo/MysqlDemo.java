d54030a07084b5c84db0ff3250d0f9edcc5208b6
gxchunhui <admin@10.16.6.183>
2016-09-12 14:35:03 +0800

    second

--- src/com/MysqlDemo/MysqlDemo.java
+++ src/com/MysqlDemo/MysqlDemo.java
@@ -12,7 +12,7 @@ public class MysqlDemo {
         String sql;
         // MySQL的JDBC URL编写方式：jdbc:mysql://主机名称：连接端口/数据库的名称?参数=值
         // 避免中文乱码要指定useUnicode和characterEncoding
-        // 执行数据库操作之前要在数据库管理系统上创建一个数据库，名字自己定，
+        // 执行数据库操作之前要在数据库管理系统上创建一个数据库，名字自己定，vbbv
         // 下面语句之前就要先创建javademo数据库
         String url = "jdbc:mysql://localhost:3306/javademo?"
                 + "user=root&password=123&useUnicode=true&characterEncoding=UTF8";
